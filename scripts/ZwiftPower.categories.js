087618682fb8efaccb737a61b2f430e4feab8daf
David Pugh <plastonick@gmail.com>
2025-08-12 13:26:15 +0100

    use the signup api which gives the user div correct as of signup

--- scripts/ZwiftPower.categories.js
+++ scripts/ZwiftPower.categories.js
@@ -2,39 +2,19 @@
 // @name         ZwiftPower
 // @namespace    http://tampermonkey.net/
 // @version      base.1.1
-// @description  Adds guessed actual categories to riders on mixed-category rides. This is limited to guessing the category for a rider based on their FTP and weight, since the real category isn't obviously accessible via any easily accessed API methods I'm aware of. zMAP is also not available either, so some users are likely to be guessed at a category under their actual category.
+// @description  Adds the riders category as of their signup to rides.
 // @author       David Pugh
 // @license      MIT
 // @match        https://zwiftpower.com/*
 // @grant        none
 // ==/UserScript==
 
-const guessCategory = data => {
-  const ftp = data.ftp
-  const weight = data.weight[0]
-  const relativeFtp = ftp / weight
-
-  if (ftp > 300 && relativeFtp > 4.6) {
-    return ['Aplus', 'A+']
-  }
-
-  if (ftp > 250 && relativeFtp > 4.2) {
-    return ['A', 'A']
-  }
-
-  if (ftp > 200 && relativeFtp > 3.36) {
-    return ['B', 'B']
-  }
-
-  if (ftp > 150 && relativeFtp > 2.626) {
-    return ['C', 'C']
-  }
-
-  if (ftp) {
-    return ['D', 'D']
-  }
-
-  return ['E', 'E']
+const categoryDivs = {
+  5: ['Aplus', 'A+'],
+  10: ['A', 'A'],
+  20: ['B', 'B'],
+  30: ['C', 'C'],
+  40: ['D', 'D']
 }
 
 function waitForRows (callback, attempts) {
@@ -64,15 +44,22 @@ function waitForRows (callback, attempts) {
   console.log('ZwiftPower script starting...')
 
   const eventId = window.location.href.match(/zid=(\d+)/)[1]
-  const res = await fetch(`cache3/results/${eventId}_view.json`)
+  const res = await fetch(`cache3/results/${eventId}_signups.json`)
   const json = await res.json()
 
   waitForRows(function () {
     console.debug('Rows detected, hydrating data')
 
-    json.data.forEach(data => {
-      const [catClass, guessedCat] = guessCategory(data)
+    if (jQuery('#pacing-category').length === 0) {
+      console.debug('Adding category header')
 
+      jQuery('#table_event_results_final thead tr')
+        .first()
+        .prepend('<th id="pacing-category" title="Based on user\'s category division at time of signup">Cat</th>')
+    }
+
+    json.data.forEach(data => {
+      const [catClass, pacerCategory] = categoryDivs[data.div] ?? ['E', 'E']
       const zwiftUserRow = jQuery(`#table_event_results_final_wrapper tbody a[href$=${data.zwid}]`).closest('tr')
 
       if (zwiftUserRow.length === 0) {
@@ -81,21 +68,13 @@ function waitForRows (callback, attempts) {
         return
       }
 
-      if (jQuery('#guessed-category').length === 0) {
-        console.debug('Adding guessed category header')
-
-        jQuery('#table_event_results_final thead tr')
-          .first()
-          .prepend('<th id="guessed-category" title="Guessed based on user\'s current FTP and weight">Cat</th>')
-      }
-
-      console.debug('Adding guessed category for user', data.zwid, data.name, guessedCat)
+      console.debug('Adding category for user', data.zwid, data.name, pacerCategory)
 
       zwiftUserRow
         .prepend(`
 <td>
   <span class="label label-cat-${catClass} label-as-badge" style="font-size:14px;">
-    ${guessedCat}
+    ${pacerCategory}
   </span>
 </td>
 `)
